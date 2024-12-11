58a4ac17c0e89a0c5d660c11d7681ed0e4e97ee6
ajithkumarpazhani5 <ajithkumarpazhani5@gmail.com>
2024-12-11 15:20:56 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,20 @@
+class demo{
+     public static void main(String args[])
+     {
+        int age = 26;
+        String name = "Ajithkumar";
+        boolean love =false;
+
+        if(love)
+        {
+
+            System.out.println(name + " is Planning to get married");
+
+        }
+
+        else{
+            System.out.println(name +" Age is " + age +", So he must focus on his career");
+        }
+     }
+
+}
